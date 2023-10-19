bb64ac4ad6c898a7e3476e6b424e1f37677a2976
tds07senai <reisdesouzawillian74@gmail.com>
2023-10-19 19:17:44 -0300

    terminando o roteamento das páginas que estão prontas

--- front-end-controle-chamada/src/app/header/header-routing.module.ts
+++ front-end-controle-chamada/src/app/header/header-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TurmaComponent } from '../turma/turma.component';
+import { NivelCursoComponent } from '../nivel-curso/nivel-curso.component';
+import { CursoComponent } from '../curso/curso.component';
+import { AlunoComponent } from '../aluno/aluno.component';
 
 const routes: Routes = [
   {
+    path: 'nivel-curso',
+    component: NivelCursoComponent    
+  },
+  {
+    path: 'curso',
+    component: CursoComponent    
+  },
+  {
     path: 'turma',
     component: TurmaComponent    
+  },
+  {
+    path: 'aluno',
+    component: AlunoComponent    
   }
 ]; 
 
